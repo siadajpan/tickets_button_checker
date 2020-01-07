0ed630aa5dc50b4b7cf3d84ceaf8d624499c82c0
Karol Misiarz <karol.misiarz@cervirobotics.com>
2020-01-07 09:38:59 +0100

    Add which site is the button found on, in sms body

--- ticket_button_checker/sms_sender.py
+++ ticket_button_checker/sms_sender.py
@@ -8,7 +8,8 @@ class SMSSender:
         self.phone = settings.PHONE
         self.client = Client(settings.Twilio.SID, settings.Twilio.TOKEN)
 
-    def send_sms(self):
+    def send_sms(self, ticket_site):
+        body = settings.SMS_TEXT + ticket_site
         self.client.messages.create(to=settings.PHONE,
                                     from_=settings.Twilio.PHONE,
-                                    body=settings.SMS_TEXT)
+                                    body=body)

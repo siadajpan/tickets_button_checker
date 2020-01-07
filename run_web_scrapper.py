daf62ce4330d8d3c07680ae4d00e0f5aa4590ed6
Karol Misiarz <karol.misiarz@cervirobotics.com>
2020-01-07 09:42:05 +0100

    Add checking one site at time, and exit condition

--- run_web_scrapper.py
+++ run_web_scrapper.py
@@ -5,15 +5,16 @@ from ticket_button_checker.sms_sender import SMSSender
 from ticket_button_checker.web_scraper import WebScraper
 
 if __name__ == '__main__':
-    ws = WebScraper(settings.SITE)
+    ws = WebScraper(settings.SITES)
     sms_sender = SMSSender()
 
     while True:
-        on_page = ws.check_on_page(settings.TEXT_SEARCHED)
+        on_page, site = ws.check_pages(settings.TEXT_SEARCHED)
 
         if on_page:
-            print('text found on page')
-            sms_sender.send_sms()
+            sms_sender.send_sms(site)
+
+        if ws.check_all_found():
             break
 
         time.sleep(10)

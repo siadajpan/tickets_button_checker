0f12efff60a028a54043d955a5d1893284e3f029
Karol Misiarz <karol.misiarz@cervirobotics.com>
2020-01-07 09:40:15 +0100

    Add multiple search for all the pages specified

--- ticket_button_checker/web_scraper.py
+++ ticket_button_checker/web_scraper.py
@@ -7,9 +7,26 @@ class WebScraper:
     def __init__(self, sites: List[str]):
         self.sites = dict([(site, False) for site in sites])
 
-    def check_on_page(self, text: str) -> bool:
-        r = requests.get(self.site)
+    @staticmethod
+    def check_on_page(site, text: str) -> bool:
+        r = requests.get(site)
         content = r.content
         on_page = content.find(bytes(text, 'UTF-8')) >= 0
 
         return on_page
+
+    def check_pages(self, text: str) -> Tuple[bool, Optional[str]]:
+        for site, found in self.sites.items():
+            if found:
+                continue
+
+            if self.check_on_page(site, text):
+                self.sites[site] = True
+                return True, site
+
+        return False, None
+
+    def check_all_found(self) -> bool:
+        all_found = all(self.sites.values())
+
+        return all_found
